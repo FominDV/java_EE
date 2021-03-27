71c78eca350eeaae587ee3d0037570df69eb4bf4
Dmitriy Fomin <79067773397@yandex.ru>
2021-03-27 19:07:31 +0300

    lesson 1

---
+++ first_java_ee/first_ee/src/main/java/servlets/FirstServlet.java
@@ -0,0 +1,38 @@
+package servlets;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class FirstServlet implements Servlet {
+
+    private transient ServletConfig config;
+
+    private final AtomicInteger requestCounter = new AtomicInteger();
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.getWriter().println("<h1>Hello from Servlet!!!</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "FirstServlet";
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
